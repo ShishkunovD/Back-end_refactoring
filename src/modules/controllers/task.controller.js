948ff482550351716466390910698f995869c6de
Dmitriy <dshishkunov.exceedteam@gmail.com>
2021-12-07 10:07:47 +0300

    fixed bugs after the first comments

--- src/modules/controllers/task.controller.js
+++ src/modules/controllers/task.controller.js
@@ -7,37 +7,35 @@ module.exports.getAllTasks = (req, res) => {
 }
 
 module.exports.createNewTask = (req, res) => {
-  if( req.body.text !== undefined && typeof req.body.isCheck === "boolean" ) {
+  if(req.body.text !== undefined && typeof req.body.isCheck === "boolean") {
     const task = new Task(req.body);
     task.save().then(result => {
       res.send({ data: result });
-    })
+    });
   } else {
     res.status(404).send("Error, all fields should be filled in correctly.");
   }
 }
 
 module.exports.changeTaskInfo = (req, res) => {
-  const params = req.query.id;
-  const body = req.body;
-  if( params !== undefined && (body.text !== undefined && typeof body.isCheck === "boolean")) {
-    Task.findByIdAndUpdate(params, { text: body.text, isCheck: body.isCheck })
-    .then(result => {
-      Task.find().then(result => res.send({ data: result }))
-    })
+  if(req.query.id !== undefined && (req.body.text !== undefined && typeof req.body.isCheck === "boolean")) {
+    const params = req.query.id;
+    const body = req.body;
+    Task.findByIdAndUpdate(params, { text: body.text, isCheck: body.isCheck }).then(result => {
+      Task.find().then(result => res.send({ data: result }));
+    });
   } else {
     res.status(404).send("Error, all fields should be filled in correctly.");
   }
 }
 
 module.exports.deleteTask = (req, res) => {
-  const identifier = req.query.id;
-  if( identifier === undefined ) {
+  if(req.query.id === undefined) {
     res.status(404).send("Error, please enter the id.");
   } else {
-    Task.deleteOne({ _id : identifier })
-    .then(result => {
+    const identifier = req.query.id;
+    Task.deleteOne({ _id : identifier }).then(result => {
       Task.find().then(result => res.send({ data: result }));
-    })
+    });
   }
 }
